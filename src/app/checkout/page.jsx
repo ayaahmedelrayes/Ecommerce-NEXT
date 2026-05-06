a3761235985ca20dbe837404b0a346f931d58220
Aya Ahmed Elrayes <ayaahmedelrayes@gmail.com>
2026-05-06 22:53:58 +0300

    Import Suspense in checkout page component

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState, Suspense } from 'react'
 import { useRouter } from 'next/navigation'
 import { useCartStore } from '@/store/cartStore'
 import { useAuth } from '@/context/AuthContext'
